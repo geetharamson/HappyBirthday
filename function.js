2ebf41eb2fa2e533b73e50483e86fdc683818989
geetharamson <47304216+geetharamson@users.noreply.github.com>
2021-07-18 08:57:21 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+//function.jsJavaScript
+alert("HERE'S THE CAKE 🎂 AND ENJOY THE PARTY 🎶🎉🎊");
+window.addEventListener('click', function() {
+    var audio = document.getElementById("HBD");
+    audio.play();
+});
